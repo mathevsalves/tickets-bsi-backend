be15a2f45d5e4cb0f2a2493ace70d972806fef65
Matheus Alves de Amorim Santos <matheus.santos@nava.com.br>
2021-12-07 19:05:25 -0300

    final

--- src/main/java/br/com/bsi/pi/ticketsbsi/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.bsi.pi.ticketsbsi.repositories;
-
-import br.com.bsi.pi.ticketsbsi.entities.Category;
-import br.com.bsi.pi.ticketsbsi.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- src/main/java/br/com/bsi/pi/ticketsbsi/repositories/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.bsi.pi.ticketsbsi.repositories;
-
-import br.com.bsi.pi.ticketsbsi.entities.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
